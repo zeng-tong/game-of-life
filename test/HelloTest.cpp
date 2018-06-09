5443143c4676ad0980a92df1c6dd4e3eae869ddb
zengtong <unixsudo@outlook.com>
2018-06-09 13:03:18 +0800

    add a match function for testing output. Reuse the code

--- test/HelloTest.cpp
+++ test/HelloTest.cpp
@@ -8,48 +8,39 @@
 // See https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md
 //     https://github.com/catchorg/Catch2/blob/master/docs/command-line.md
 
-TEST_CASE("should return 'hello world' when call method hello()", "[hello_test]") {
-
-    auto message = hello();
-
-    REQUIRE(message == "hello world");
-
+void match(std::vector<std::vector<int>>& source, std::vector<std::vector<int>>& target, int i = 0, int j = 0) {
+    REQUIRE(source.size() == target.size());
+    for (; i < source.size(); ++i) {
+        for (; j < source[i].size(); ++j) {
+            REQUIRE(source[i][j] == target[i][j]);
+        }
+    }
 }
 
 TEST_CASE("input all dead, should return all dead when call game_of_life(). Not concern edge") {
     std::vector<std::vector<int>> matrix(5, std::vector<int>(5, 0));
 
     auto result = game_of_life(matrix);
-    for (int i = 1; i < 5; ++i) {
-        for (int j = 1; j < 5; ++j) {
-            REQUIRE(result[i][j] == 0);
-        }
-    }
+    match(result, matrix, 1, 1);
 }
 
 TEST_CASE("input all dead, Now concern edge cell. should return all dead when call game_of_life()") {
     std::vector<std::vector<int>> matrix(5, std::vector<int>(5, 0));
     auto result = game_of_life(matrix);
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            REQUIRE(result[i][j] == 0);
-        }
-    }
+    match(result,matrix);
 }
 
 TEST_CASE("input all live") {
     std::vector<std::vector<int>> input(5, std::vector<int>(5, 1));
 
-    std::vector<std::vector<int>> output(5, std::vector<int>(5, 0));
-    output[0][0] = output[0][4] =  output[4][0] = output[4][4] = 1;
+    std::vector<std::vector<int>> target(5, std::vector<int>(5, 0));
+    target[0][0] = target[0][4] =  target[4][0] = target[4][4] = 1;
 
 
-    auto result = game_of_life(input);
-    printMatrix(result);
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-//            std::cout << "i = " << i << ", j = " << j << " result[i][j] = "<< result[i][j] << std::endl;
-            REQUIRE(result[i][j] == output[i][j]);
-        }
-    }
+    auto output = game_of_life(input);
+    match(output, target);
+
+
+    printMatrix(output);
+
 }
